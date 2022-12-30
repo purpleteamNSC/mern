2eeff0e04c86fb6717a1f7b54574c3d00c27b1e3
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2022-12-30 23:30:46 +0000

    refactor config route

--- API-BREAKINGNEWS/index.js
+++ API-BREAKINGNEWS/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const userRoute = require("./src/routes/user.route");
 const app = express();
+
+const userRoute = require("./src/routes/user.route");
 
 const port = 3000
 
+app.use(express.json());
 app.use("/user", userRoute);
 
 app.listen(port, ()=> console.log(`\n Servidor rodando na porta ${port}`));
--- API-BREAKINGNEWS/src/controllers/user.controller.js
+++ API-BREAKINGNEWS/src/controllers/user.controller.js
@@ -1,8 +1,22 @@
-const create = (req, res)=>{
-    const user = req.body;
+const create = (req, res) => {
+    const {name, username, email, password, avatar, background} = req.body;
 
-    console.log(user)
-}
+    if(!name || !username || !email || !password || !avatar || !background){
+        res.status(400).send({message: "Submit all fields for registration"})
+    }
+
+    res.status(201).send({
+        message: "User created succesfully",
+        user: {
+            name,
+            username,
+            email,
+            
+            avatar,
+            background,
+        },
+    });
+};
 
 module.exports = { create };
 
